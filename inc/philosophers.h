467f9b117ab67051aeb4fce43d6e21110a28d56f
LawlietR9 <137063744+LawlietR9@users.noreply.github.com>
2025-01-18 19:10:45 +0900

    Add files via upload

---
+++ inc/philosophers.h
@@ -0,0 +1,73 @@
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+# include <limits.h>
+
+typedef struct s_philo
+{
+	struct s_data	*data;
+	pthread_t		thread;
+	int				n_philo;
+	int				n_meals;
+	u_int64_t		time_death;
+	u_int64_t		time_eat;
+	u_int64_t		time_sleep;
+	int				id;
+	int				eat_count;
+	int				status;
+	int				eating;
+	uint64_t		time_to_die;
+	pthread_mutex_t	lock;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
+}	t_philo;
+
+typedef struct s_data
+{
+	pthread_t		*thread;
+	int				dead;
+	int				finished;
+	int 			is_over;
+	int				n_philo;
+	int				n_meals;
+	t_philo			*philo;
+	u_int64_t		time_death;
+	u_int64_t		time_eat;
+	u_int64_t		time_sleep;
+	u_int64_t		time_start;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t end;
+	pthread_mutex_t	lock;
+	pthread_mutex_t	write;
+}	t_data;
+
+// main.c
+int ft_exit(t_data *data);
+int ft_error(char *str, t_data *data);
+int check_args(char **argv);
+
+// init.c
+int init_forks(t_data *data);
+int init_philo(t_data *data);
+int init_data(int argc, char **argv, t_data *data);
+
+// utils.c
+int ft_strlen(char *str);
+int ft_atoi(char *str);
+int print_message(t_philo *philo, char *message);
+size_t get_time(void);
+void	ft_usleep(size_t ms);
+
+// routine.c
+void *routine(void *philo_pointer);
+void *check_death(void *pointer);
+int is_dead(t_philo *philo);
+int eat(t_philo *philo);
+
+
+
+#endif
